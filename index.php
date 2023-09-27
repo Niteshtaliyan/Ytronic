33d69394dcb776a1512bef4313744340bd71e9e1
Nitesh Taliyan <talyan.nitesh98@gmail.com>
2023-09-27 15:07:37 +0530

    second

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
-/**
- * Front to the Ytronic website application.
- *
- * @package Ytronic
- */
 
-require 'yt-config.php';
-
-require_once YTTMPL .'/functions.php';
-
-load_template();
+echo 'Hello, World!';
--- yt-template/assets/js/main.js
+++
@@ -1,5 +0,0 @@
-function submitForm(event)
-{
-	event.preventDefault();
-	alert('Form blocked. Edit file: /yt-template/assets/js/main.js');
-}
